13a6a701ac324336efc402c194accfdccfbca793
nazareno diaz mauro <diaznazareno66@gmail.com>
2022-06-23 15:28:38 -0300

    Archivos de prueba

---
+++ archivo-ejemplo.js
@@ -0,0 +1,5 @@
+// variable : es darle un valor a algo, el cual luego me facilitara la busqueda y el uso del mismo;
+//strings : son un conjunto de caracteres (letras o numeros) los cuales se ubican entre comillas ;
+// funciones:( agrupan un codigo el cual coy a repetir muchas veces. esto se guarda en un lugar para que luego puedas utilizarlo; return: sirve para elegir que me devolvera el comando cuando yo ejecute esta funcion);
+// declaraciones if: sirve como condicionante, la funcion puede devolver una cosa solo si(if) se cumple tal requisito;
+// valores booleanos: true: es lo que te deculeve el comando cuando lo que escribis es verdadero; false: es lo que devuelve el comando cuando lo que decis esta equivocado. Podes darle valores a las cosas afirmando si son true o false.
